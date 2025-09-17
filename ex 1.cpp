1440e4219959d1401533701676fa6e8cc2a69a78
njoki26187-sudo <njoki.26187@students.kyu.ac.ke>
2025-09-17 12:49:44 +0300

    Add files via upload

---
+++ ex 1.cpp
@@ -0,0 +1,19 @@
+/*
+name:njoki victor kairu
+reg number:CT100/G/26187/25
+*/
+
+#include <stdio.h>
+
+int main()
+{
+printf("mathematics for science.\n");
+printf("essentials of accounting.\n");
+printf("introduction to programming and algolithms.\n");
+printf("communication skills.\n");
+printf("introduction to computer applications.\n");
+printf("analog electronics.\n");
+printf("fundamentals of ICT.\n");
+printf("computer systems and organisations.\n");	
+return 0;
+}
